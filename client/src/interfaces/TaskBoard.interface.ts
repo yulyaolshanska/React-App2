d70be6795466adf7b4dffeaf686dc06315e77e7c
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-04-05 19:36:03 +0300

    feat: client/Home page
    1. handleDeleteTaskBoard
    2. focusInput
    3. handleAddNewBoard
    4. DropDown
    5. BoardCard

--- client/src/interfaces/TaskBoard.interface.ts
+++ client/src/interfaces/TaskBoard.interface.ts
@@ -1,4 +1,4 @@
 export interface TaskBoard {
-  id?: number;
+  id: number;
   title: string;
 }
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,14 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import BoardCard from "../../components/boardCard/BoardCart";
+import DropDown from "../../components/dropDown/DropDown";
 import { TaskBoard } from "../../interfaces/TaskBoard.interface";
 import { useAppDispatch } from "../../redux/store";
-import { getAllTaskBoards } from "../../redux/taskBoard/taskBoardAsyncThunk";
+import {
+  addTaskBoard,
+  deleteTaskBoard,
+  getAllTaskBoards,
+} from "../../redux/taskBoard/taskBoardAsyncThunk";
 import { selectTaskBoards } from "../../redux/taskBoard/taskBoardSelectors";
 import styles from "./Home.module.scss";
 
 const HomePage: React.FC = () => {
   const dispatch = useAppDispatch();
+  const [activeTitleInput, setActiveTitleInput] = useState<number | null>(null);
 
   useEffect(() => {
     dispatch(getAllTaskBoards());
@@ -16,11 +22,43 @@ const HomePage: React.FC = () => {
 
   const boards: TaskBoard[] = useSelector(selectTaskBoards);
 
+  const handleDeleteTaskBoard = (id: number) => {
+    dispatch(deleteTaskBoard(id));
+  };
+
+  const focusInput = (id: number) => {
+    setActiveTitleInput(id);
+    const input = document.getElementById(
+      `input-${id}`
+    ) as HTMLInputElement | null;
+    if (input) {
+      input.focus();
+    }
+  };
+
+  const handleAddNewBoard = () => {
+    const maxId = Math.max(...boards.map((board) => board.id), 0);
+    const newId = maxId + 1;
+    const newTaskBoard: TaskBoard = {
+      id: newId,
+      title: "New Board",
+    };
+    dispatch(addTaskBoard(newTaskBoard));
+  };
+
   return (
     <div className={styles.pageContainer}>
-      <button>Add new board</button>
+      <button onClick={handleAddNewBoard}>Add new board</button>
       {boards &&
-        boards.map((board) => <BoardCard key={board.id} board={board} />)}
+        boards.map((board) => (
+          <BoardCard key={board.id} board={board}>
+            <DropDown
+              onEditClick={() => focusInput(board.id)}
+              onDeleteClick={() => handleDeleteTaskBoard(board.id)}
+              mode="task"
+            />
+          </BoardCard>
+        ))}
     </div>
   );
 };
