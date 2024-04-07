c215b0c8f606424f86138fe75e06cf27649bf5d1
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-04-07 23:47:16 +0300

    remove taskCard stories

--- client/src/redux/taskList/taskListSelectors.ts
+++ client/src/redux/taskList/taskListSelectors.ts
@@ -2,7 +2,7 @@ import { RootState } from "../store";
 
 export const selectTaskLists = (state: RootState) => state.taskLists.taskLists;
 
-export const selectTaskListsLoading = (state: RootState) =>
+export const selectTaskListsLoading = (state: RootState): boolean =>
   state.taskLists.loading;
 
 export const selectTaskListsError = (state: RootState) => state.taskLists.error;
