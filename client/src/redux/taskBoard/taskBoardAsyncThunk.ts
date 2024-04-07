7be542ea1baa0b71c27c50a6e08ee5f40410043e
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-04-07 12:56:56 +0300

    refactor: try/catch for taskBoardAsyncThunk

--- client/src/redux/taskBoard/taskBoardAsyncThunk.ts
+++ client/src/redux/taskBoard/taskBoardAsyncThunk.ts
@@ -5,26 +5,36 @@ import { TaskBoard } from "../../interfaces/TaskBoard.interface";
 export const getAllTaskBoards = createAsyncThunk<TaskBoard[]>(
   "taskBoards/fetchTaskBoards",
   async () => {
-    const response = await fetch(`${BASE_URL}/task-board`);
-    const data = await response.json();
+    try {
+      const response = await fetch(`${BASE_URL}/task-board`);
+      const data = await response.json();
 
-    return data;
+      return data;
+    } catch (error) {
+      console.error("Error get all task board:", error);
+      throw error;
+    }
   }
 );
 
 export const addTaskBoard = createAsyncThunk(
   "taskBoards/addTaskBoard",
   async (newTaskBoard: TaskBoard) => {
-    const response = await fetch(`${BASE_URL}/task-board`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newTaskBoard),
-    });
-    const data = await response.json();
+    try {
+      const response = await fetch(`${BASE_URL}/task-board`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newTaskBoard),
+      });
+      const data = await response.json();
 
-    return data;
+      return data;
+    } catch (error) {
+      console.error("Error add new task board:", error);
+      throw error;
+    }
   }
 );
 
@@ -56,10 +66,15 @@ export const updateTaskBoard = createAsyncThunk<
 export const deleteTaskBoard = createAsyncThunk<number, number>(
   "taskBoards/deleteTaskBoard",
   async (id) => {
-    await fetch(`${BASE_URL}/task-board/${id}`, {
-      method: "DELETE",
-    });
+    try {
+      await fetch(`${BASE_URL}/task-board/${id}`, {
+        method: "DELETE",
+      });
 
-    return id;
+      return id;
+    } catch (error) {
+      console.error("Error get all task board:", error);
+      throw error;
+    }
   }
 );
